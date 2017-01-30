fa9d023427fe8fed5d643a6578a7d5f7983c67e4
J.R <jrlockwood@ets.org>
2017-01-30 14:40:13 -0500

    added ustat_deg2

---
+++ R/ustat_deg2.R
@@ -0,0 +1,65 @@
+ustat_deg2 <- function(x, psi){## compute degree k=2 U-statistic based on (n x p) data matrix "x" and
+    ## function "psi", along with analytical and asymptotic variance.
+    if(!is.numeric(x)){
+        stop("x must be numeric")
+    }
+    
+    if(any(is.na(x))){
+        stop("missing values in 'x' not allowed")
+    }
+    
+    if(!is.matrix(x)){
+        x <- matrix(x, ncol=1)
+    }
+    n <- nrow(x)
+    
+    if(n < 2){
+        stop("there must be at least two observations")
+    }
+    
+    if(!is.function(psi)){
+        stop("psi must be a symmetric function accepting two arguments concordant with rows of x")
+    }
+    
+    if(abs(psi(x[1,],x[2,]) - psi(x[2,],x[1,])) > 1e-8){
+        stop("psi does not appear to be a symmetric function based on first two elements of x")
+    }
+    
+    ij      <- t(combn(1L:n, 2))
+    psivals <- apply(ij, 1, function(.ij){ psi(x[.ij[1],], x[.ij[2],]) })
+    
+    ## compute U-statistic
+    U       <- mean(psivals)
+    
+    ## compute terms needed to estimate exact and asymptotic variance.
+    ##
+    ## NOTE: the terms involving pairs with exactly one element in common are tricky to
+    ## track.  we loop over choose(n,2) pairs and for each such target pair, we
+    ## find the 2*(n-2) pairs that share exactly one element in common with it.
+    ## thus there are a total of n(n-1)(n-2) products involved.  the covariance is computed
+    ## by the mean of the relevant products minus the square of the marginal mean of psi,
+    ## all times (n)/(n-1) to make it closer to the unbiased estimator of the true covariance.
+    ## we estimate the variance of psi using a similar degrees of freedom correction.
+    ss2     <- (mean(psivals^2) - U^2) * (n/(n-1))
+    ss1     <- 0.0
+    ind     <- 0L
+    for(istar in 1:(n-1)){
+        for(jstar in (istar+1):n){
+            ind <- ind + 1
+            ## cat(paste("\n\n###########",istar,jstar,"##########\n"))
+            ## stopifnot( (istar == ij[ind,1]) && (jstar == ij[ind,2]) )
+            ## print(ij[setdiff(which( ((ij[,1] == istar) | (ij[,2] == istar)) | ((ij[,1] == jstar) | (ij[,2] == jstar)) ), ind),])
+            others <- setdiff(which( ((ij[,1] == istar) | (ij[,2] == istar)) | ((ij[,1] == jstar) | (ij[,2] == jstar)) ), ind)
+            ss1 <- ss1 + sum(psivals[ind] * psivals[others])
+        }
+    }
+    ss1  <- ((ss1 / (n * (n-1) * (n-2))) - U^2) * (n/(n-1))
+    
+    return(list(psi         = psi,
+                psivals     = psivals,
+                U           = U,
+                ss1         = ss1,
+                ss2         = ss2,
+                varU.exact  = (1 / choose(n,2)) * ((2 * (n-2) * ss1) + ss2),
+                varU.asymp  = 4*ss1 / n))
+}
