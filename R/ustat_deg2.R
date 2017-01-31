899f1efabbfb10f0a57d352ce51835c4bb8e259b
J.R <jrlockwood@ets.org>
2017-01-30 22:04:43 -0500

    change variance calc in ustat_deg2

--- R/ustat_deg2.R
+++ R/ustat_deg2.R
@@ -1,5 +1,8 @@
-ustat_deg2 <- function(x, psi){## compute degree k=2 U-statistic based on (n x p) data matrix "x" and
+ustat_deg2 <- function(x, psi){
+    ## compute degree k=2 U-statistic based on (n x p) data matrix "x" and
     ## function "psi", along with analytical and asymptotic variance.
+    ## NOTE: variance calculations are degenerate unless n >= 4 I think.
+    
     if(!is.numeric(x)){
         stop("x must be numeric")
     }
@@ -36,12 +39,12 @@ ustat_deg2 <- function(x, psi){## compute degree k=2 U-statistic based on (n x p
     ## NOTE: the terms involving pairs with exactly one element in common are tricky to
     ## track.  we loop over choose(n,2) pairs and for each such target pair, we
     ## find the 2*(n-2) pairs that share exactly one element in common with it.
-    ## thus there are a total of n(n-1)(n-2) products involved.  the covariance is computed
-    ## by the mean of the relevant products minus the square of the marginal mean of psi,
-    ## all times (n)/(n-1) to make it closer to the unbiased estimator of the true covariance.
-    ## we estimate the variance of psi using a similar degrees of freedom correction.
-    ss2     <- (mean(psivals^2) - U^2) * (n/(n-1))
-    ss1     <- 0.0
+    ## thus there are a total of n(n-1)(n-2) products involved.
+    ##
+
+    ## see handnotes for derivations of unbiased estimators for various pieces
+    ## "Epp" denotes the E[psi(X1,X2)*psi(X1,X3)] - product of pairs sharing one element.    
+    Epp     <- 0.0
     ind     <- 0L
     for(istar in 1:(n-1)){
         for(jstar in (istar+1):n){
@@ -50,14 +53,21 @@ ustat_deg2 <- function(x, psi){## compute degree k=2 U-statistic based on (n x p
             ## stopifnot( (istar == ij[ind,1]) && (jstar == ij[ind,2]) )
             ## print(ij[setdiff(which( ((ij[,1] == istar) | (ij[,2] == istar)) | ((ij[,1] == jstar) | (ij[,2] == jstar)) ), ind),])
             others <- setdiff(which( ((ij[,1] == istar) | (ij[,2] == istar)) | ((ij[,1] == jstar) | (ij[,2] == jstar)) ), ind)
-            ss1 <- ss1 + sum(psivals[ind] * psivals[others])
+            Epp <- Epp + sum(psivals[ind] * psivals[others])
         }
     }
-    ss1  <- ((ss1 / (n * (n-1) * (n-2))) - U^2) * (n/(n-1))
+    Epp  <- Epp / (n * (n-1) * (n-2))
+
+    ## get estimates of other pieces needed for variance calculation
+    musq <- (1.0 / choose(n-2,2)) * ( (choose(n,2) * U^2) - mean(psivals^2) - 2*(n-2)*Epp )
+    ss1  <-             Epp - musq
+    ss2  <- mean(psivals^2) - musq
     
     return(list(psi         = psi,
                 psivals     = psivals,
                 U           = U,
+                Epp         = Epp,
+                musq        = musq,
                 ss1         = ss1,
                 ss2         = ss2,
                 varU.exact  = (1 / choose(n,2)) * ((2 * (n-2) * ss1) + ss2),
